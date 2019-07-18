97f5d3c0d056d415d4723fba86016c947f0212ac
yimd85 <yimd85@gmail.com>
2019-07-17 20:52:36 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { CSVLink } from "react-csv";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: [
+      { firstname: "Ahmed", lastname: "Tomi", email: "ah@smthing.co.com" },
+      { firstname: "Raed", lastname: "Labes", email: "rl@smthing.co.com" },
+      { firstname: "Yezzi", lastname: "Min l3b", email: "ymin@cocococo.com" }
+    ],
+    headers: [
+      { test: "First Name", key: "firstname" },
+      { test: "Last Name", key: "lastname" },
+      { test: "Email", key: "email" }
+    ]
+  }
+  render() {
+    return (
+      < div >
+        <CSVLink data={this.state.data} headers={this.state.headers}>
+          Download me
+</CSVLink>;
+</div >
+    )
+  }
 }
 
 export default App;
